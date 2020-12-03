e8cabce21205117fb9093b391fd980ae2f6b85b1
Zach Chan <zacharyleechan@gmail.com>
2020-12-03 14:52:43 -0800

    added users and favorites to psql and cql schemas

--- schema.sql
+++ schema.sql
@@ -1,16 +1,25 @@
 
-CREATE SCHEMA trulia
+CREATE SCHEMA trelia
   CREATE TABLE listings (
-    id integer PRIMARY KEY,
+    listing_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
     price varchar(14),
-    size_bd integer,
-    size_ba integer,
+    size_bd smallint CONSTRAINT valid_bd_count CHECK (size_bd > 0 AND size_bd < 100),
+    size_ba smallint CONSTRAINT valid_ba_count CHECK (size_ba > 0 AND size_ba < 100),
     address varchar(50),
     neighborhood varchar(50),
-    image varchar(100)
+    image varchar(100),
+    favorite boolean
   );
   CREATE TABLE similar_homes (
-    listing_id integer references trulia.listings(id),
-    similar_id integer references trulia.listings(id),
-    similarity_weight integer
+    listing_id uuid references trelia.listings(listing_id),
+    similar_id uuid references trelia.listings(listing_id),
+    similarity_weight decimal
   );
+  CREATE TABLE users (
+    user_id uuid PRIMARY KEY
+  )
+  CREATE TABLE user_favorites (
+    user_id uuid references trelia.users(user_id),
+    list_name varchar(50),
+    favorite_id uuid references trelia.listings(listing_id)
+  )
