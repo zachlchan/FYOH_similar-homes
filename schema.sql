1b49601125e7f7a8cfeff7c67533cbd481a9e89d
Zach Chan <zacharyleechan@gmail.com>
2020-12-03 21:02:28 -0800

    first draft of approved schemas

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 
 CREATE SCHEMA trelia
   CREATE TABLE listings (
-    listing_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    listing_id serial PRIMARY KEY,
     price varchar(14),
     size_bd smallint CONSTRAINT valid_bd_count CHECK (size_bd > 0 AND size_bd < 100),
     size_ba smallint CONSTRAINT valid_ba_count CHECK (size_ba > 0 AND size_ba < 100),
@@ -11,15 +11,15 @@ CREATE SCHEMA trelia
     favorite boolean
   );
   CREATE TABLE similar_homes (
-    listing_id uuid references trelia.listings(listing_id),
-    similar_id uuid references trelia.listings(listing_id),
+    listing_id serial references trelia.listings(listing_id),
+    similar_id serial references trelia.listings(listing_id),
     similarity_weight decimal
   );
   CREATE TABLE users (
-    user_id uuid PRIMARY KEY
+    user_id serial PRIMARY KEY
+    user_name varchar(30)
   )
   CREATE TABLE user_favorites (
-    user_id uuid references trelia.users(user_id),
-    list_name varchar(50),
-    favorite_id uuid references trelia.listings(listing_id)
+    user_id serial references trelia.users(user_id),
+    favorite_id serial references trelia.listings(listing_id)
   )
