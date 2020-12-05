d7884017f3acefd634313deb104cfcb161c05c0e
Zach Chan <zacharyleechan@gmail.com>
2020-12-05 14:51:18 -0800

    implemented seed function for users table

--- db_psql/seed.js
+++ db_psql/seed.js
@@ -4,8 +4,10 @@ const db = require('./connection.js');
 // ---- to create schema in the db, in terminal enter:
 // ---- \i shema.sql
 
+// file paths for csv's
 const listing_data_path = path.join(__dirname, '../data_generator/csv/psql_listing_data.csv');
-const similar_homes_path = path.join(__dirname, '../data_generator/csv/psql_similar_home_data.csv');
+const similar_homes_data_path = path.join(__dirname, '../data_generator/csv/psql_similar_home_data.csv');
+const user_data_path = path.join(__dirname, '../data_generator/csv/psql_user_data.csv');
 
 // seed data into listings table from csv
 db.client.query(
@@ -16,17 +18,29 @@ db.client.query(
     } else {
       console.log('listings data successfully seeded');
     }
-  },
+  }
 );
 
 // seed data into similar_homes table from csv
 db.client.query(
-  `COPY trelia.similar_homes(listing_id, similar_id, similarity_weight) FROM '${similar_homes_path}' DELIMITER ',' CSV HEADER`,
+  `COPY trelia.similar_homes(listing_id, similar_id, similarity_weight) FROM '${similar_homes_data_path}' DELIMITER ',' CSV HEADER`,
   (err, res) => {
     if (err) {
       console.log('error seeding psql', err);
     } else {
-      console.log('similar_homes data successfully seeded')
+      console.log('similar_homes data successfully seeded');
     }
   }
-)
+);
+
+// seed data into users table from csv
+db.client.query(
+  `COPY trelia.users(user_name) FROM '${user_data_path}' CSV HEADER`,
+  (err, res) => {
+    if (err) {
+      console.log('error seeding psql', err);
+    } else {
+      console.log('user data successfully seeded');
+    }
+  }
+);
