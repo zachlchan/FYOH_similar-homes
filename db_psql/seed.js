b36caee854ddda89203fd53283c52e49f9032845
Zach Chan <zacharyleechan@gmail.com>
2020-12-05 14:35:10 -0800

    implemented seed function for similar_homes table

--- db_psql/seed.js
+++ db_psql/seed.js
@@ -1,19 +1,32 @@
 const path = require('path');
 const db = require('./connection.js');
 
-// to load schema, in terminal enter:
-// \i shema.sql
+// ---- to create schema in the db, in terminal enter:
+// ---- \i shema.sql
 
 const listing_data_path = path.join(__dirname, '../data_generator/csv/psql_listing_data.csv');
-console.log('seed path', listing_data_path);
+const similar_homes_path = path.join(__dirname, '../data_generator/csv/psql_similar_home_data.csv');
 
+// seed data into listings table from csv
 db.client.query(
   `COPY trelia.listings(price, size_bd, size_ba, size_sqft, street_address, neighborhood, listing_image, favorite) FROM '${listing_data_path}' DELIMITER ',' CSV HEADER`,
   (err, res) => {
     if (err) {
-      console.log('postgres seed error', err);
+      console.log('error seeding psql', err);
     } else {
-      console.log('listings success');
+      console.log('listings data successfully seeded');
     }
   },
 );
+
+// seed data into similar_homes table from csv
+db.client.query(
+  `COPY trelia.similar_homes(listing_id, similar_id, similarity_weight) FROM '${similar_homes_path}' DELIMITER ',' CSV HEADER`,
+  (err, res) => {
+    if (err) {
+      console.log('error seeding psql', err);
+    } else {
+      console.log('similar_homes data successfully seeded')
+    }
+  }
+)
