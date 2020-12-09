1f37655356284f88843ecddfb3f7e2fff53eec87
Zach Chan <zacharyleechan@gmail.com>
2020-12-08 17:13:06 -0800

    fixed success cb string

--- db_psql/seed.js
+++ db_psql/seed.js
@@ -11,13 +11,13 @@ const user_data_path = path.join(__dirname, '../data_generator/csv/psql_user_dat
 const user_favorites_data_path = path.join(__dirname, '../data_generator/csv/psql_user_favorites_data.csv');
 
 // helper function for seeding data into tables
-const seed_table = (queryString, cbString) => {
+const seed_table = (queryString, cb) => {
   db.client.query(queryString,
     (err, res) => {
       if (err) {
         console.log('error seeding psql', err);
       } else {
-        cbString;
+        cb;
       }
     }
   );
@@ -33,7 +33,7 @@ const users_queryString = `COPY trelia.users(user_name) FROM '${user_data_path}'
 const user_favorites_queryString = `COPY trelia.user_favorites(user_id, favorite_id) FROM '${user_favorites_data_path}' DELIMITER ',' CSV HEADER`;
 
 // seed the tables
-seed_table(listings_queryString, console.timeEnd('seeding listings data'));
-seed_table(similar_homes_queryString, console.timeEnd('seeding similar_homes data'));
-seed_table(users_queryString, console.timeEnd('seeding users data'));
-seed_table(user_favorites_queryString, console.timeEnd('seeding user_favorites data'));
+seed_table(listings_queryString, console.log('seeding listings data'));
+seed_table(similar_homes_queryString, console.log('seeding similar_homes data'));
+seed_table(users_queryString, console.log('seeding users data'));
+seed_table(user_favorites_queryString, console.log('seeding user_favorites data'));
