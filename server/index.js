59e0b7a493edf1f444ce454d06f371f5d7ff33a8
Zach Chan <zacharyleechan@gmail.com>
2020-12-10 10:40:32 -0800

    renamed server.js to index.js, re-named router to controller and connected it to psql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const listingRouter = require('./routers/listing.js');
+const controller = require('../controller/listing.js');
 
 const app = express();
 const port = 8030;
@@ -10,7 +10,8 @@ app.use(express.json());
 app.use('/carousel/:id', express.static(dist));
 
 // get all similar listings when given a specific id
-app.get('*/:id/listing', listingRouter.getOne);
+// app.get('*/:id/listing', listingRouter.getOne);
+app.get('similar-homes', controller.getSimilar);
 
 app.listen(8030, () => {
   console.log(`listening on http://localhost:${port}`);
