217d0436b00e5d95a1bc2fe6b7e709d5a3df2e3b
Zach Chan <zacharyleechan@gmail.com>
2020-12-09 23:30:11 -0800

    refactored so a single select query would return 15 similar listings with all info

--- data_generator/theBigDataGen_cql.js
+++ data_generator/theBigDataGen_cql.js
@@ -4,9 +4,6 @@ const faker = require('faker');
 // number of primary listings = 10M
 let numListings = 10000000;
 
-// number of similar homes = 15 per listing
-let numSimilarHomes = 15
-
 // users = 1M
 let numUsers = 1000000;
 
@@ -55,44 +52,51 @@ const generateFavoriteListings = () => {
 
 // generate data for home_listings_by_id table
 const writeHomeListingData = fs.createWriteStream('./data_generator/csv/cql_home_listings_data.csv');
-writeHomeListingData.write('listing_id|price|size_bd|size_ba|size_sqft|street_address|neighborhood|listing_image|favorite|similar_homes\n');
+writeHomeListingData.write('listing_id|price|size_bd|size_ba|size_sqft|street_address|neighborhood|listing_image|favorite|similar_to_id|similarity_weight\n');
 
 const writeHomeListings = (writer, callback) => {
-  let i = numListings;
-  let id = 1;
+  let i = 1
+  let totalListings = numListings * 15; // 15 similar per listing
+  let similar_id = numListings;
+  let similar_counter = 0;
 
   const write = () => {
     let ok = true;
     do {
-       // track progress fo every 1M rows
-       if (i % 1000000 === 0) {
+      // track progress fo every 1M rows
+      if (i % 1000000 === 0) {
         console.log(`${i} rows remaining`);
       }
 
-      i -= 1;
-
-      const listing_id = id;
-      const price = getRandomIntInclusive(20, 800, 2) * 10000;
-      const size_bd = getRandomIntInclusive(1, 6, 0.5);
-      const size_ba = getRandomIntInclusive(1, 6);
-      const size_sqft = getRandomIntInclusive(12,40, 2) * 100;
-      const street_address = faker.address.streetAddress();
-      const neighborhood = faker.fake("{{address.county}}, {{address.city}}, {{address.stateAbbr}}");
-      const img_num = i % 1000;
-      const listing_image = `https://sdc-trelia.s3-us-west-1.amazonaws.com/${img_num}.jpg`;
-      const favorite = false;
-      const similar_homes = generateSimilarHomes();
-
-      const data = `${listing_id}|${price}|${size_bd}|${size_ba}|${size_sqft}|${street_address}|${neighborhood}|${listing_image}|${favorite}|{${similar_homes}}\n`;
-
-      if (i === 0) {
-        writer.write(data, callback);
+      // increment id every 15 listings
+      if (similar_counter === 15) {
+        similar_id -= 1;
+        similar_counter = 0;
       } else {
-        ok = writer.write(data);
-      }
+        const listing_id = i;
+        const price = getRandomIntInclusive(20, 800, 2) * 10000;
+        const size_bd = getRandomIntInclusive(1, 6, 0.5);
+        const size_ba = getRandomIntInclusive(1, 6);
+        const size_sqft = getRandomIntInclusive(12,40, 2) * 100;
+        const street_address = faker.address.streetAddress();
+        const neighborhood = faker.fake("{{address.county}}, {{address.city}}, {{address.stateAbbr}}");
+        const img_num = i % 1000;
+        const listing_image = `https://sdc-trelia.s3-us-west-1.amazonaws.com/${img_num}.jpg`;
+        const favorite = false;
+        const similar_to_id = similar_id;
+        const similarity_weight = (getRandomIntInclusive(100, 1000, 0.5) * 0.01).toFixed(2);
 
-      id += 1;
-    } while (i > 0 && ok);
+        const data = `${listing_id}|${price}|${size_bd}|${size_ba}|${size_sqft}|${street_address}|${neighborhood}|${listing_image}|${favorite}|${similar_to_id}|${similarity_weight}\n`;
+
+        if (i === totalListings) {
+          writer.write(data, callback);
+        } else {
+          ok = writer.write(data);
+        }
+        i += 1;
+        similar_counter += 1;
+      }
+    } while (i <= totalListings && ok); // 15 similar homes per listing
 
     if (i > 0) {
       writer.once('drain', write);
@@ -109,15 +113,15 @@ writeHomeListings(writeHomeListingData, () => {
 
 
 // generate data for favorite_listings table
-const writeFavoriteListingsData = fs.createWriteStream('./data_generator/csv/cql_favorite_listings_data.csv');
-writeFavoriteListingsData.write('user_id|user_name|favorite_homes\n');
+// const writeFavoriteListingsData = fs.createWriteStream('./data_generator/csv/cql_favorite_listings_data.csv');
+// writeFavoriteListingsData.write('user_id|user_name|favorite_homes\n');
 
 const writeFavoriteListings = (writer, callback) => {
   let i = numUsers;
+  let id = 1;
 
   const write = () => {
     let ok = true;
-    let id = 1;
 
     do {
       i -= 1;
@@ -145,8 +149,8 @@ const writeFavoriteListings = (writer, callback) => {
 
   write();
 }
-console.time('generated favorite_listings data');
-writeFavoriteListings(writeFavoriteListingsData, () => {
-  writeFavoriteListingsData.end();
-  console.timeEnd('generated favorite_listings data');
-});
+// console.time('generated favorite_listings data');
+// writeFavoriteListings(writeFavoriteListingsData, () => {
+//   writeFavoriteListingsData.end();
+//   console.timeEnd('generated favorite_listings data');
+// });
