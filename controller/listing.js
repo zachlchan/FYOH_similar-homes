0dbc3cb7addaaa3a649f1cfda0f428e1469e9fd6
Zach Chan <zacharyleechan@gmail.com>
2020-12-10 11:30:49 -0800

    implemented post route for adding a similar home to a listing

--- controller/listing.js
+++ controller/listing.js
@@ -2,8 +2,8 @@ const db = require('../db_psql/connection.js');
 
 // Get request for all the similar homes associated with a listing
 exports.getSimilar = (req, res) => {
-  const listing_id = req.body.listing_id;
-  console.log('request body', req.body);
+  const { listing_id } = req.body;
+  console.log('GET, request body', req.body);
 
   db.client.query(`SELECT trelia.listings.listing_id, price, size_bd, size_ba, size_sqft, street_address, neighborhood, listing_image, favorite, similarity_weight FROM trelia.listings LEFT JOIN trelia.similar_homes ON trelia.listings.listing_id = trelia.similar_homes.similar_id WHERE trelia.similar_homes.listing_id = ${listing_id};`, (err, similarListings) => {
     if (err) {
@@ -13,3 +13,17 @@ exports.getSimilar = (req, res) => {
     }
   });
 }
+
+exports.addSimilar = (req, res) => {
+  const { listing_id, similar_id, similarity_weight } = req.body;
+  console.log('POST, request body', req.body);
+
+  db.client.query(`INSERT INTO trelia.similar_homes(listing_id, similar_id, similarity_weight) VALUES (${listing_id}, ${similar_id}, ${similarity_weight});`, (err) => {
+    if (err) {
+      return handleError(err);
+    } else {
+      res.sendStatus(201);
+    }
+  });
+}
+
--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ app.use('/carousel/:id', express.static(dist));
 // get all similar listings when given a specific id
 // app.get('*/:id/listing', listingRouter.getOne);
 app.get('/similar-homes', controller.getSimilar);
+// add a similar listing to a listing id
+app.post('/similar-homes', controller.addSimilar);
 
 app.listen(8030, () => {
   console.log(`listening on http://localhost:${port}`);
